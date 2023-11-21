708955a17b29a5d5ddfee643cd37f3b7e4271aae
karwsz <karwszpl@gmail.com>
2023-11-21 09:41:00 +0100

    Playing with listeners, trying to understand how Swing text components work

--- src/main/java/pl/canthideinbush/akashaquesteditor/app/dynamic/popups/Popups.java
+++ src/main/java/pl/canthideinbush/akashaquesteditor/app/dynamic/popups/Popups.java
@@ -5,7 +5,13 @@ import pl.canthideinbush.akashaquesteditor.app.Application;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
 import java.awt.*;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.InputMethodListener;
 
 public class Popups {
 
@@ -25,19 +31,45 @@ public class Popups {
         
         JTextPane pane = new JTextPane();
         pane.setBorder(new LineBorder(dialog.getBackground(), 15));
+        pane.getDocument().addUndoableEditListener(new UndoableEditListener() {
+            @Override
+            public void undoableEditHappened(UndoableEditEvent e) {
+                System.out.println("Undoable");
+            }
+        });
+
+        pane.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                System.out.println("insert");
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                System.out.println("remove");
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                System.out.println("changed");
+            }
+        });
 
         gbc.fill = GridBagConstraints.BOTH;
         gbc.weightx = 0.8;
         gbc.weighty = 1;
 
 
-        dialog.add(pane, gbc);
+        JScrollPane scrollPane = new JScrollPane(pane, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        dialog.add(scrollPane, gbc);
 
         JPanel confirmPanel = new JPanel();
         gbc.weightx = 0.2;
 
         dialog.add(confirmPanel, gbc);
-
+        confirmPanel.setPreferredSize(confirmPanel.getSize());
+        confirmPanel.setMinimumSize(confirmPanel.getPreferredSize());
 
         dialog.pack();
         dialog.setVisible(true);
@@ -45,7 +77,7 @@ public class Popups {
 
     private static JDialog templateDialog(String title) {
         JDialog dialog = new JDialog(Application.instance.frame, title, true);
-        dialog.setPreferredSize(new Dimension(500, 325));
+        dialog.setPreferredSize(new Dimension(600, 450));
         dialog.setIconImage(Application.icon);
         dialog.setResizable(false);
         dialog.setLocationRelativeTo(Application.instance);
