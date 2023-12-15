be25f83dc5167c7e3a4ed29ff52e6c7d8d3b36ef
karwsz <karwszpl@gmail.com>
2023-12-15 13:13:07 +0100

    Started InstructionsTable.InstructionBlock class

--- src/main/java/pl/canthideinbush/akashaquesteditor/app/components/quest/InstructionsTable.java
+++ src/main/java/pl/canthideinbush/akashaquesteditor/app/components/quest/InstructionsTable.java
@@ -17,7 +17,36 @@ public class InstructionsTable extends JPanel {
     }
 
     private void initialize() {
+        setLayout(new GridLayout());
         setBackground(Color.LIGHT_GRAY);
+    }
+
+    public void update() {
+
+    }
+
+
+    public static class InstructionBlock extends JPanel {
+
+        private final String instruction;
+        private final GridBagConstraints constraints;
+        private JTextField nameField;
+
+        public InstructionBlock(String name, String instruction) {
+            setLayout(new GridBagLayout());
+            constraints = new GridBagConstraints();
+            setName(name);
+            this.instruction = instruction;
+            initializeComponents();
+        }
+
+        private void initializeComponents() {
+            constraints.fill = GridBagConstraints.BOTH;
+            constraints.weightx = 0.8;
+            constraints.weighty = 1;
+            nameField = new JTextField();
+        }
+
     }
 
 }
